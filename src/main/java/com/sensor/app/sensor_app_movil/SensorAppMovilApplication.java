3469adea9fd84d7a16a1015e347c6c0c3ec14a32
ATMatias22 <matias.torrez@davinci.edu.ar>
2023-03-10 17:31:06 -0300

    Creando la estructura del proyecto

---
+++ src/main/java/com/sensor/app/sensor_app_movil/SensorAppMovilApplication.java
@@ -0,0 +1,13 @@
+package com.sensor.app.sensor_app_movil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorAppMovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorAppMovilApplication.class, args);
+	}
+
+}
