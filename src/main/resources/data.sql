be8de0e4b3ce293323ff7c7f7d9fd70eaa9bd52b
ATMatias22 <matias.torrez@davinci.edu.ar>
2023-11-02 01:07:09 -0300

    Se agregó el data.sql para inicializar valores

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+/* la password es "Password123%"*/
+INSERT INTO users(
+	 country, date_of_birth, email, enabled, lastname, name, password)
+	VALUES ('Argentina','1991-01-01','davinciproyectosensor@gmail.com',true,'Jim','Jim','$2a$10$gTrVzE2JEKDnDyuD7vUOEeLF.4fdVJxP2EwwRax5Tz.guA6HpZ2Bq');
+
+
